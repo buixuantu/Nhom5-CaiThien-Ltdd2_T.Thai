846d405f950dc51be7d0f3ccef74eb6e4c937d66
buixuantu <buixuantu@users.noreply.github.com>
2019-05-23 11:44:17 +0700

    NguyenTruongDuy

---
+++ NguyenTruongDuy-17211TT3508/EXE4/main/java/com/example/a/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.a;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button, button2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.buttonCall);
+        button2 = (Button) findViewById(R.id.btnSms);
+        VideoView videoView = findViewById(R.id.video_view);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SENDTO);
+                intent.putExtra("sms_body", "");
+                intent.setData(Uri.parse("sms:Bun Bo Hue" ));
+                startActivity(intent);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0377778888"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.bunbohue;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+}
